f799c65962c895c0cfc879a81e0d5603f0285624
Malwry <125805960+malwry@users.noreply.github.com>
2025-12-11 11:02:01 +1100

    fixed cbridge not being enabled by default

--- src/main/java/com/github/ozmeyham/imsbridge/utils/ConfigUtils.java
+++ src/main/java/com/github/ozmeyham/imsbridge/utils/ConfigUtils.java
@@ -48,8 +48,8 @@ public class ConfigUtils {
         combinedBridgeEnabled    = cfg.getBoolean(
                 "combinedBridgeEnabled",
                 Configuration.CATEGORY_GENERAL,
-                false,
-                "Enable combined bridge chat"
+                true,
+                "Enable combined bridge messages"
         );
         bridgeEnabled            = cfg.getBoolean(
                 "bridgeEnabled",
@@ -61,7 +61,7 @@ public class ConfigUtils {
                 "combinedBridgeChatEnabled",
                 Configuration.CATEGORY_GENERAL,
                 false,
-                "Enable combined chat mode"
+                "Enable combined chat chat"
         );
         firstLogin = cfg.getBoolean(
                 "firstLogin",
